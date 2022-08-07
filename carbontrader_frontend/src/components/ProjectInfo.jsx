7f7beea07fb2b7c7f0a3675f7db0ad128ae11673
enriquegutierrezi <gutierrez_enrique@javeriana.edu.co>
2022-08-06 23:50:35 -0500

    fixes due to request

--- carbontrader_frontend/src/components/ProjectInfo.jsx
+++ carbontrader_frontend/src/components/ProjectInfo.jsx
@@ -1,21 +1,19 @@
 import React, { useState } from 'react';
 import '../styles/ProjectInfo.scss';
 import Img from'../assets/img/ProjectExample.jpg';
-const ProjectInfo = () => {
+const ProjectInfo = (props) => {
     return(
         <section className='InfoContainer'>
             <div className='InfoContainer-descriptionContainer'>
-                <img src={Img} alt="" />
-                <h1>Proyecto Delta</h1>
+                <img src={props.imageURL} alt="" />
+                <h1>{props.title}</h1>
                 <div className='InfoContainer-textContainer'>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad distinctio eius aperiam odio est nesciunt, praesentium quae facere, earum blanditiis molestiae labore? Impedit animi labore, commodi porro nam consequatur cupiditate.</p>
+                    <p>{props.description}</p>
                     <h2>¿Quienes somos?</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ut dolorum molestiae enim corporis. Ut maxime aspernatur quaerat soluta, deleniti in sint consequuntur, dicta veritatis reprehenderit possimus corrupti eos debitis?</p>
+                    <p>{props.description}</p>
                     <h2>¿Donde nos ubicamos?</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit fugiat sint, non inventore voluptate commodi repellat, sed labore, voluptates odit magni quis! Veritatis tempora earum explicabo voluptate officiis maxime! Eum.</p>
-                </div>            
-                
-                             
+                    <p>{props.description}</p>
+                </div>                           
             </div>  
             <div className='InfoContainer-buttonContainer'>
                 <button>Comprar créditos</button>
--- carbontrader_frontend/src/routes/App.jsx
+++ carbontrader_frontend/src/routes/App.jsx
@@ -4,11 +4,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProjectInfo from '../components/ProjectInfo'
 
 function App() {
-  
+  const project = {
+    "title": "Proyecto alfa",
+    "imageURL": "https://alkilautos.com/blog/wp-content/uploads/2019/03/D.jpg",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla molestie, lorem sit amet dignissim viverra, est dolor dapibus dui, quis aliquet neque erat commodo arcu.",
+}
   return (
     <BrowserRouter>
         <Routes>
-        <Route exact path="/" element={<ProjectInfo/>} />
+        <Route exact path="/" element={<ProjectInfo imageURL = {project.imageURL} title = {project.title} description = {project.description}/>} />
         </Routes>
     
 </BrowserRouter>

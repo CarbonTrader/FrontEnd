bf05f738a88ca0cb533466d53f6c6bce57f24a74
enriquegutierrezi <71357961+enriquegutierrezi@users.noreply.github.com>
2022-08-08 11:52:36 -0500

    Project UI (#2)
    * Project UI
    
    * fixes due to request

--- carbontrader_frontend/src/routes/App.jsx
+++ carbontrader_frontend/src/routes/App.jsx
@@ -1,16 +1,24 @@
-import React from "react";
 
+
+import '../styles/App.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "../pages/Home";
 import '../styles/global.scss';
+import ProjectInfo from '../components/ProjectInfo'
 
-
-const App = () => {
+function App() {
+  const project = {
+    "title": "Proyecto alfa",
+    "imageURL": "https://alkilautos.com/blog/wp-content/uploads/2019/03/D.jpg",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla molestie, lorem sit amet dignissim viverra, est dolor dapibus dui, quis aliquet neque erat commodo arcu.",
+  }
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={<ProjectInfo imageURL={project.imageURL} title={project.title} description={project.description} />} />
       </Routes>
+
     </BrowserRouter>
   );
 }
